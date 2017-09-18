91b394fb5e92ff8be027664ef1b72e78ed5ceab0
Michael Bachmann <m.bachmann@salzbrenner.com>
2017-09-18 13:32:17 +0200

    showing Refs

--- src/main/java/net/bbmsoft/jgitfx/modules/RepositoryTableVisualizer.java
+++ src/main/java/net/bbmsoft/jgitfx/modules/RepositoryTableVisualizer.java
@@ -3,12 +3,16 @@ package net.bbmsoft.jgitfx.modules;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.api.errors.NoHeadException;
 import org.eclipse.jgit.lib.PersonIdent;
+import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
 
@@ -18,7 +22,9 @@ import javafx.scene.control.TableView;
 
 public class RepositoryTableVisualizer {
 
-	private TableView<RevCommit> table;
+	private final TableView<RevCommit> table;
+	private final Map<String, List<Ref>> refMap;
+
 	private Repository repository;
 
 	public RepositoryTableVisualizer(TableView<RevCommit> table, TableColumn<RevCommit, String> branchColumn,
@@ -26,10 +32,11 @@ public class RepositoryTableVisualizer {
 			TableColumn<RevCommit, String> timeColumn) {
 
 		this.table = table;
+		this.refMap = new HashMap<>();
 
-//		 branchColumn.setCellValueFactory(cdf -> {
-//		 return new SimpleStringProperty(cdf.getValue().getBranch());
-//		 });
+		branchColumn.setCellValueFactory(cdf -> {
+			return new SimpleStringProperty(getRefs(cdf.getValue()));
+		});
 
 		commitMessageColumn.setCellValueFactory(cdf -> {
 			return new SimpleStringProperty(cdf.getValue().getShortMessage());
@@ -42,6 +49,25 @@ public class RepositoryTableVisualizer {
 		timeColumn.setCellValueFactory(cdf -> {
 			return new SimpleStringProperty(toString(new Date(cdf.getValue().getCommitTime() * 1000L)));
 		});
+	}
+
+	private String getRefs(RevCommit commit) {
+
+		String id = commit.getId().getName();
+		List<Ref> refs = this.refMap.get(id);
+		if (refs != null) {
+			StringBuilder sb = new StringBuilder();
+			Iterator<Ref> it = refs.iterator();
+			while (it.hasNext()) {
+				sb.append(it.next().getName());
+				if (it.hasNext()) {
+					sb.append(", ");
+				}
+			}
+			return sb.toString();
+		}
+
+		return "";
 	}
 
 	private String toString(Date date) {
@@ -76,6 +102,23 @@ public class RepositoryTableVisualizer {
 		// TODO possibly move to background thread if too slow
 
 		Git git = Git.wrap(this.repository);
+		
+		this.refMap.clear();
+		
+		List<Ref> allRefs = new ArrayList<>();
+		allRefs.addAll(git.branchList().call());
+		allRefs.addAll(git.tagList().call());
+
+		allRefs.forEach(ref -> {
+			String id = ref.getObjectId().getName();
+			List<Ref> refs = this.refMap.get(id);
+			if (refs == null) {
+				refs = new ArrayList<>();
+				this.refMap.put(id, refs);
+			}
+			refs.add(ref);
+		});
+
 		Iterable<RevCommit> commitsIterable = git.log().all().call();
 
 		List<RevCommit> commits = new ArrayList<>();
